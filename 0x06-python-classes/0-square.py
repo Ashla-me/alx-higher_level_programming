f2c5eb54de3acff799e2eb565c80fd3dfdfc1b53
Ashlame <mercyashlamesunday@gmail.com>
2024-01-23 07:38:54 -0800

    empty class

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
-
+"""
+class Square: defines an empty square
+"""
 class Square:
+    """
+    An empty square that does nothing
+    """
     def __init__(self):
         pass
 my_square = Square()
