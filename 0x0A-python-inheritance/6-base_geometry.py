36b288959584128b32750a3abe86dd8aca611bb2
Ashlame <mercyashlamesunday@gmail.com>
2024-02-05 13:09:57 -0800

    A class with an attribute

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+A BaseGeometry
+"""
+
+
+class BaseGeometry
+"""A class with a public attribute area"""
+def area(self):
+    """raise Exception"""
+    raise Exception("area() is not implemented")
