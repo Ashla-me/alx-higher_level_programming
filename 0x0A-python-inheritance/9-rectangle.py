e39f2f4c0dbb97553d74cca2c5dbae99ebc2fd07
Ashlame <mercyashlamesunday@gmail.com>
2024-02-06 03:43:37 -0800

    inherits the class

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+subclass Rectangle
+"""
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """Sub class"""
+    def __init__(self, width, height):
+        """instantiation of the rectangle"""
+        super().__init__()
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
+
+    def area(self):
+        return self.__width * self.__height
+
+    def __str__(self):
+        return f"[Rectangle] {self.__width}/{self.__height}"
+
+    def print(self):
+        print(self.__str__())
