3478f0be9203426afe220fc481dd5032bb15c310
Ashlame <mercyashlamesunday@gmail.com>
2024-02-06 06:50:03 -0800

    inherit from square

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+subclass of a square
+"""
+
+
+Rectangle=__import__('9-rectangle').Rectangle
+
+
+
+class Square(Rectangle):
+    """subclass"""
+
+    def __init__(self, size):
+        """instantiate a subclass"""
+
+        super().__init__(size, size)
+        self.__size = size
+        self.integer_validator('size', size)
+    def area(self):
+        return self.__size ** 2
+
+    def __str__(self):
+        return f"[Square] {self.__size}/{self.__size}"
